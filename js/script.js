baedd4ed3e073d0f779a8fff892908a26cb7c578
diamant1991 <diamant1988@mail.ru>
2015-11-04 17:02:39 +0200

    add

--- js/script.js
+++ js/script.js
@@ -10,6 +10,76 @@ $(document).ready(function() {
       }, 1000);
     });
 
+	//resize function
+	var mq = window.matchMedia( "(max-width: 991px)" );
+
+	if(mq.matches){
+		$('.nav-list').addClass('accardeon_list');
+		$('.nav-list > li').addClass('accardeon_item');
+		$('.nav-list > li > a').addClass('accardeon_trigger');
+		$('.nav-list > li > ul').addClass('accardeon_inner');
+		$('.nav-list > li > ul > li').addClass('accardeon_inner_item');
+	}
+	else{
+		$('.nav-list').removeClass('accardeon_list');
+		$('.nav-list > li').removeClass('accardeon_item');
+		$('.nav-list > li > a').removeClass('accardeon_trigger');
+		$('.nav-list > li > a + ul').removeClass('accardeon_inner');
+		$('.nav-list > li > ul > li').removeClass('accardeon_inner_item');
+	}
+
+	$( window ).resize(function() {
+    	wnd_resize()
+	});
+	wnd_resize()
+	
+	function wnd_resize(){
+		if($( window ).width()<971){
+			$('.nav-list').addClass('accardeon_list');
+			$('.nav-list > li').addClass('accardeon_item');
+			$('.nav-list > li > a').addClass('accardeon_trigger');
+			$('.nav-list > li > ul').addClass('accardeon_inner');
+			$('.nav-list > li > ul > li').addClass('accardeon_inner_item');
+		}
+		else{
+			$('.nav-list').removeClass('accardeon_list');
+			$('.nav-list > li').removeClass('accardeon_item');
+			$('.nav-list > li > a').removeClass('accardeon_trigger');
+			$('.nav-list > li > a + ul').removeClass('accardeon_inner');
+			$('.nav-list > li > ul > li').removeClass('accardeon_inner_item');
+		}
+	}
+// end resize function
+
+
+
+	//accardeon
+	$('.accardeon_trigger').click(function(e) {
+		e.preventDefault()
+
+		var $this = $(this),
+				item = $this.closest('.accardeon_item'),
+				list = $this.closest('.accardeon_list'),
+				items = list.find('.accardeon_item'),
+				content = item.find('.accardeon_inner'),
+				otherContent = list.find('.accardeon_inner'),
+				duration = 300;
+
+			if(!item.hasClass('active')){
+				items.removeClass('active');
+				item.addClass('active');
+				otherContent.stop(true).slideUp(duration);
+				content.stop(true).slideDown(duration);
+			}
+			else{
+				content.stop(true).slideUp(duration);
+				item.removeClass('active');
+			}
+	});
+
+	
+	// end accardeon
+
 });
 
 jQuery(document).ready(function(){
